7ee14fa16a46ae30334bf08d0c27e3695e1452af
sandrinnio <sandrokakashvili@gmail.com>
2021-10-17 15:59:48 +0400

    feat: advice generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { SlipData } from "./interfaces/slip.interface";
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState<string>();
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  const getAdvice = async () => {
+    try {
+      const { data }: { data: SlipData } = await axios.get(
+        "https://api.adviceslip.com/advice"
+      );
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={getAdvice}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
     </div>
   );
 }
---
+++ src/interfaces/slip.interface.ts
@@ -0,0 +1,8 @@
+export interface SlipData {
+  slip: Slip;
+}
+
+interface Slip {
+  advice: string;
+  id: number;
+}
